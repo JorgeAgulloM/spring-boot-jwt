c204f50ea1fafd7da4ea57bf097c51ac177261c2
Jorge1 <agullojorge@gmail.com>
2023-11-14 07:07:21 +0100

    Added ClienteDao

---
+++ src/main/java/com/softyorch/cursospring/app/models/dao/ClienteDaoImpl.java
@@ -0,0 +1,26 @@
+package com.softyorch.cursospring.app.models.dao;
+
+import java.util.List;
+
+import org.springframework.stereotype.Repository;
+
+import com.softyorch.cursospring.app.models.entity.Cliente;
+
+import jakarta.persistence.EntityManager;
+import jakarta.persistence.PersistenceContext;
+import jakarta.transaction.Transactional;
+
+@Repository
+public class ClienteDaoImpl implements IClienteDao {
+
+	@PersistenceContext
+	private EntityManager em;
+	
+	@SuppressWarnings("unchecked")
+	@Transactional()
+	@Override
+	public List<Cliente> findeAll() {
+		return em.createQuery("from Cliente").getResultList();
+	}
+
+}
---
+++ src/main/java/com/softyorch/cursospring/app/models/dao/IClienteDao.java
@@ -0,0 +1,11 @@
+package com.softyorch.cursospring.app.models.dao;
+
+import java.util.List;
+
+import com.softyorch.cursospring.app.models.entity.Cliente;
+
+public interface IClienteDao {
+
+	public List<Cliente> findeAll();
+	
+}
