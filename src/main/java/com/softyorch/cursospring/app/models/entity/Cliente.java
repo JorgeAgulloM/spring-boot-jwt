f43d0ae1b10f7b710d76522a81f0d672015ee08e
Jorge1 <agullojorge@gmail.com>
2023-11-14 13:51:02 +0100

    Added prePersist for createAt

--- src/main/java/com/softyorch/cursospring/app/models/entity/Cliente.java
+++ src/main/java/com/softyorch/cursospring/app/models/entity/Cliente.java
@@ -3,14 +3,7 @@ package com.softyorch.cursospring.app.models.entity;
 import java.io.Serializable;
 import java.util.Date;
 
-import jakarta.persistence.Column;
-import jakarta.persistence.Entity;
-import jakarta.persistence.GeneratedValue;
-import jakarta.persistence.GenerationType;
-import jakarta.persistence.Id;
-import jakarta.persistence.Table;
-import jakarta.persistence.Temporal;
-import jakarta.persistence.TemporalType;
+import jakarta.persistence.*;
 
 @Entity
 @Table(name = "clientes")
@@ -30,6 +23,11 @@ public class Cliente implements Serializable {
 	@Column(name = "create_at")
 	@Temporal(TemporalType.DATE)
 	private Date createAt;
+
+	@PrePersist
+	public void prePersist() {
+		createAt = new Date();
+	}
 
 	public Long getId() {
 		return id;
