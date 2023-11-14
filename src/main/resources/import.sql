63b14e5d110b3dfe4a9e471c5246cdeb71832ca2
Jorge1 <agullojorge@gmail.com>
2023-11-14 08:43:10 +0100

    Added html clientView

---
+++ src/main/resources/import.sql
@@ -0,0 +1,3 @@
+/* Populate tables*/
+INSERT INTO clientes (id, nombre, apellido, email, create_at) VALUES(1, 'Jorge', 'Agulló', 'jorge@agullo.com', '2023-08-28')
+INSERT INTO clientes (id, nombre, apellido, email, create_at) VALUES(2, 'John', 'Doe', 'john@doe.com', '2023-08-28')
