643d67ca47b7249d09303d399899010991c55724
Jorge1 <agullojorge@gmail.com>
2023-11-17 18:44:37 +0100

    Added Product entity

---
+++ src/main/java/com/softyorch/cursospring/app/models/entity/Producto.java
@@ -0,0 +1,63 @@
+package com.softyorch.cursospring.app.models.entity;
+
+import jakarta.persistence.*;
+import org.springframework.format.annotation.DateTimeFormat;
+
+import java.io.Serializable;
+import java.util.Date;
+
+@Entity
+@Table(name = "productos")
+public class Producto implements Serializable {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    @Column(name = "nombre")
+    private String nombre;
+    @Column(name = "precio")
+    private Double precio;
+    @Column(name = "create_at")
+    @Temporal(TemporalType.DATE)
+    @DateTimeFormat(pattern = "yyyy-MM-dd")
+    private Date createAt;
+
+    @PrePersist
+    public void prePersist() {
+        createAt = new Date();
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public Double getPrecio() {
+        return precio;
+    }
+
+    public void setPrecio(Double precio) {
+        this.precio = precio;
+    }
+
+    public Date getCreatedAt() {
+        return createAt;
+    }
+
+    public void setCreatedAt(Date createdAt) {
+        this.createAt = createdAt;
+    }
+
+    private static final long serialVersionUID = 1L;
+}
