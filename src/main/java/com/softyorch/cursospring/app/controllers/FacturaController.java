6ea8d923c7c51ac7ec6cb9bd09d16c75dd8ad26d
Jorge1 <agullojorge@gmail.com>
2023-11-18 08:06:44 +0100

    (WIP) Creating FacturaController

---
+++ src/main/java/com/softyorch/cursospring/app/controllers/FacturaController.java
@@ -0,0 +1,49 @@
+package com.softyorch.cursospring.app.controllers;
+
+import com.softyorch.cursospring.app.models.entity.Cliente;
+import com.softyorch.cursospring.app.models.entity.Factura;
+import com.softyorch.cursospring.app.service.IClienteService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.SessionAttributes;
+import org.springframework.web.servlet.mvc.support.RedirectAttributes;
+
+import java.util.Map;
+
+@Controller
+@RequestMapping(value = "/factura")
+@SessionAttributes("factura")
+public class FacturaController {
+
+
+    @Autowired
+    private IClienteService clienteService;
+
+    @GetMapping("/form/{cliente_id}")
+    public String create(
+            @PathVariable(value = "cliente_id") Long clienteId,
+            Map<String, Object> model,
+            RedirectAttributes flash
+    ) {
+
+        Cliente cliente = clienteService.findOne(clienteId);
+
+        if (cliente == null) {
+            flash.addFlashAttribute("error", "El cliente no existe en la base de datos");
+            return "redirect:/listar";
+        }
+
+        Factura factura = new Factura();
+        factura.setCliente(cliente);
+
+        model.put("factura", factura);
+        model.put("tilte", "Crear Factura");
+
+        return "factura/from";
+    }
+
+
+}
