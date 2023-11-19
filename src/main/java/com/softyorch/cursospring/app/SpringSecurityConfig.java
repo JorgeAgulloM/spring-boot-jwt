ed02b51ed405c52d5d287605cea1d89fa98d1a20
Jorge1 <agullojorge@gmail.com>
2023-11-19 18:55:35 +0100

    refator

--- src/main/java/com/softyorch/cursospring/app/SpringSecurityConfig.java
+++ src/main/java/com/softyorch/cursospring/app/SpringSecurityConfig.java
@@ -1,19 +1,14 @@
 package com.softyorch.cursospring.app;
 
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.Customizer;
-import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
-import org.springframework.security.config.annotation.web.configuration.WebSecurityConfiguration;
 import org.springframework.security.core.userdetails.User;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
-import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.provisioning.InMemoryUserDetailsManager;
 import org.springframework.security.web.SecurityFilterChain;
-import org.springframework.security.web.util.matcher.AntPathRequestMatcher;
 
 @Configuration
 public class SpringSecurityConfig {
@@ -27,13 +22,13 @@ public class SpringSecurityConfig {
     public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
 
         http.authorizeHttpRequests(auth -> auth
-                .requestMatchers("/", "/css/**", "/js/**", "/images/**", "/listar").permitAll()
-                .requestMatchers("/detail/**").hasAnyRole("USER")
-                .requestMatchers("/uploads/**").hasAnyRole("USER")
-                .requestMatchers("/form/**").hasAnyRole("ADMIN")
-                .requestMatchers("/eliminar/**").hasAnyRole("ADMIN")
-                .requestMatchers("/factura/**").hasAnyRole("ADMIN")
-                .anyRequest().authenticated())
+                        .requestMatchers("/", "/css/**", "/js/**", "/images/**", "/listar").permitAll()
+                        .requestMatchers("/detail/**").hasAnyRole("USER")
+                        .requestMatchers("/uploads/**").hasAnyRole("USER")
+                        .requestMatchers("/form/**").hasAnyRole("ADMIN")
+                        .requestMatchers("/eliminar/**").hasAnyRole("ADMIN")
+                        .requestMatchers("/factura/**").hasAnyRole("ADMIN")
+                        .anyRequest().authenticated())
                 .formLogin(fl -> fl.loginPage("/login").permitAll())
                 .logout(lOut -> lOut
                         .invalidateHttpSession(true)
@@ -64,9 +59,6 @@ public class SpringSecurityConfig {
 
         return manager;
     }
-
-
-
 
 
 }
