6b671c048eb09826bca372b478c747f01e6e1dc0
Jorge1 <agullojorge@gmail.com>
2023-11-19 12:29:10 +0100

    (WIP) Adding security. Added login view

--- src/main/java/com/softyorch/cursospring/app/SpringSecurityConfig.java
+++ src/main/java/com/softyorch/cursospring/app/SpringSecurityConfig.java
@@ -3,6 +3,7 @@ package com.softyorch.cursospring.app;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfiguration;
@@ -54,6 +55,7 @@ public class SpringSecurityConfig {
                     .logoutSuccessUrl("/login?logout")
                     .permitAll();
         });
+        http.httpBasic(Customizer.withDefaults());
 
         return http.build();
     }
