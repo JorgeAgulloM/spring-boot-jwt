bd9a40346abced0a2629c9735ea758c5529e5adf
Jorge1 <agullojorge@gmail.com>
2023-11-19 13:00:48 +0100

    (WIP) Adding security. Added login error message

--- src/main/java/com/softyorch/cursospring/app/controllers/LoginController.java
+++ src/main/java/com/softyorch/cursospring/app/controllers/LoginController.java
@@ -3,6 +3,7 @@ package com.softyorch.cursospring.app.controllers;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
 import java.security.Principal;
@@ -11,11 +12,20 @@ import java.security.Principal;
 public class LoginController {
 
     @GetMapping("/login")
-    public String login(Model model, Principal principal, RedirectAttributes flash) {
+    public String login(
+            @RequestParam(value = "error", required = false) String error,
+            Model model,
+            Principal principal,
+            RedirectAttributes flash
+    ) {
 
         if (principal != null) {
             flash.addFlashAttribute("info","Ya tienes una sesión iniciada!");
             return "redirect:/";
+        }
+
+        if (error != null) {
+            model.addAttribute("error", "Error en de Login: Nombre de usuario o constraseña incorrectos. Por favor, vuelva a intentarlo");
         }
 
         return "login";
