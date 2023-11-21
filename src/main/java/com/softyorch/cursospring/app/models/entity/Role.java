83248d0f6925b007e7493413f03e257c6a426275
Jorge1 <agullojorge@gmail.com>
2023-11-21 05:00:33 +0100

    (WIP) Adding security. Changing security to JPA Authentication. Crated User & Role Entities.

---
+++ src/main/java/com/softyorch/cursospring/app/models/entity/Role.java
@@ -0,0 +1,33 @@
+package com.softyorch.cursospring.app.models.entity;
+
+
+import jakarta.persistence.*;
+
+@Entity
+@Table(name = "authorities", uniqueConstraints = {@UniqueConstraint(columnNames = {"user_id", "authority"})})
+public class Role {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private String authority;
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getAuthority() {
+        return authority;
+    }
+
+    public void setAuthority(String authority) {
+        this.authority = authority;
+    }
+
+    private static final long serialVersionUID = 1L;
+}
