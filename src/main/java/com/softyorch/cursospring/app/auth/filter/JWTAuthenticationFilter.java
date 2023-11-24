b15bc17a73f91c81a6c496dff94a1d5f88d9bdec
Jorge1 <agullojorge@gmail.com>
2023-11-24 12:54:20 +0100

    (WIP) Created jwt.

--- src/main/java/com/softyorch/cursospring/app/auth/filter/JWTAuthenticationFilter.java
+++ src/main/java/com/softyorch/cursospring/app/auth/filter/JWTAuthenticationFilter.java
@@ -1,13 +1,25 @@
 package com.softyorch.cursospring.app.auth.filter;
 
+import com.fasterxml.jackson.databind.ObjectMapper;
+import io.jsonwebtoken.Jwts;
+import io.jsonwebtoken.io.Decoders;
+import io.jsonwebtoken.security.Keys;
+import jakarta.servlet.FilterChain;
+import jakarta.servlet.ServletException;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
 import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
 import org.springframework.security.core.Authentication;
 import org.springframework.security.core.AuthenticationException;
+import org.springframework.security.core.userdetails.User;
 import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
 import org.springframework.security.web.util.matcher.AntPathRequestMatcher;
+
+import javax.crypto.SecretKey;
+import java.io.IOException;
+import java.util.HashMap;
+import java.util.Map;
 
 public class JWTAuthenticationFilter extends UsernamePasswordAuthenticationFilter {
 
@@ -39,5 +51,35 @@ public class JWTAuthenticationFilter extends UsernamePasswordAuthenticationFilte
                 UsernamePasswordAuthenticationToken.unauthenticated(username, password);
 
         return authenticationManager.authenticate(authToken);
+    }
+
+    @Override
+    protected void successfulAuthentication(
+            HttpServletRequest request,
+            HttpServletResponse response,
+            FilterChain chain,
+            Authentication authResult
+    ) throws IOException, ServletException {
+
+        //String username = authResult.getName();
+        User user = (User) authResult.getPrincipal();
+        String username = user.getUsername();
+
+        SecretKey secretKey = Keys.hmacShaKeyFor(Decoders.BASE64.decode("MiclaveSecreta123456789MiclaveSecreta123456789"));
+        logger.warn("SecretKey: ".concat(secretKey.getAlgorithm()));
+
+        String token = Jwts.builder().subject(username).signWith(secretKey).compact();
+        logger.warn("Token: ".concat(token));
+
+        response.addHeader("Authorization", "Bearer ".concat(token));
+
+        Map<String, Object> body = new HashMap<>();
+        body.put("token", token);
+        body.put("user", user);
+        body.put("message", String.format("Hola %s, lo has conseguido!", username));
+
+        response.getWriter().write(new ObjectMapper().writeValueAsString(body));
+        response.setStatus(200);
+        response.setContentType("application/json");
     }
 }
