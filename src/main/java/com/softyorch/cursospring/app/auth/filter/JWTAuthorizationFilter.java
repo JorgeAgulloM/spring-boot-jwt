22455edf9d13bf8048fdc38a69ec4b5883c7a8c1
Jorge1 <agullojorge@gmail.com>
2023-11-24 17:33:38 +0100

    (WIP) the token is retrieved, analysed & validated.

--- src/main/java/com/softyorch/cursospring/app/auth/filter/JWTAuthorizationFilter.java
+++ src/main/java/com/softyorch/cursospring/app/auth/filter/JWTAuthorizationFilter.java
@@ -1,13 +1,27 @@
 package com.softyorch.cursospring.app.auth.filter;
 
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.softyorch.cursospring.app.auth.SimpleGrantedAuthoritiesMixin;
+import io.jsonwebtoken.Claims;
+import io.jsonwebtoken.JwtException;
+import io.jsonwebtoken.Jwts;
+import io.jsonwebtoken.io.Decoders;
+import io.jsonwebtoken.security.Keys;
 import jakarta.servlet.FilterChain;
 import jakarta.servlet.ServletException;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
 import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.security.web.authentication.www.BasicAuthenticationFilter;
 
+import javax.crypto.SecretKey;
 import java.io.IOException;
+import java.util.Arrays;
+import java.util.Collection;
 
 public class JWTAuthorizationFilter extends BasicAuthenticationFilter {
     public JWTAuthorizationFilter(AuthenticationManager authenticationManager) {
@@ -22,6 +36,39 @@ public class JWTAuthorizationFilter extends BasicAuthenticationFilter {
             chain.doFilter(request, response);
             return;
         }
+
+        SecretKey secret = Keys.hmacShaKeyFor(Decoders.BASE64.decode("MiclaveSecreta123456789MiclaveSecreta123456789"));
+
+        boolean tokenValid = false;
+        Claims token = null;
+        try {
+            token = Jwts.parser()
+                    .verifyWith(secret)
+                    .build()
+                    .parseSignedClaims(header.replace("Bearer ", ""))
+                    .getPayload();
+            tokenValid = true;
+        } catch (JwtException | IllegalArgumentException e) {
+               e.fillInStackTrace();
+        }
+
+        UsernamePasswordAuthenticationToken auth = null;
+        if (tokenValid) {
+            String username = token.getSubject();
+            Object roles = token.get("authorities"); //authority
+
+            Collection<? extends GrantedAuthority> authorities = Arrays.asList(
+                    new ObjectMapper()
+                            .addMixIn(SimpleGrantedAuthority.class, SimpleGrantedAuthoritiesMixin.class)
+                            .readValue(roles.toString().getBytes(), SimpleGrantedAuthority[].class)
+            );
+
+            auth = new UsernamePasswordAuthenticationToken(username, null, authorities);
+        }
+
+        SecurityContextHolder.getContext().setAuthentication(auth);
+        chain.doFilter(request, response);
+
     }
 
     protected boolean requiresAuthentication(String header) {
