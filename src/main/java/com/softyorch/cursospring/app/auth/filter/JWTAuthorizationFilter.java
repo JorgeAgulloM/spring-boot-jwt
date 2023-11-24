14f860818a195bf701df6f70696d02e03cb1be4e
Jorge1 <agullojorge@gmail.com>
2023-11-24 16:06:49 +0100

    (WIP) Added AuthenticationFiler.

---
+++ src/main/java/com/softyorch/cursospring/app/auth/filter/JWTAuthorizationFilter.java
@@ -0,0 +1,30 @@
+package com.softyorch.cursospring.app.auth.filter;
+
+import jakarta.servlet.FilterChain;
+import jakarta.servlet.ServletException;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.web.authentication.www.BasicAuthenticationFilter;
+
+import java.io.IOException;
+
+public class JWTAuthorizationFilter extends BasicAuthenticationFilter {
+    public JWTAuthorizationFilter(AuthenticationManager authenticationManager) {
+        super(authenticationManager);
+    }
+
+    @Override
+    protected void doFilterInternal(HttpServletRequest request, HttpServletResponse response, FilterChain chain) throws IOException, ServletException {
+        String header = request.getHeader("Authorization");
+
+        if (!requiresAuthentication(header)) {
+            chain.doFilter(request, response);
+            return;
+        }
+    }
+
+    protected boolean requiresAuthentication(String header) {
+        return header != null && header.startsWith("Bearer ");
+    }
+}
