ac5da230aaeb92e27ee9c21529a287ffbc7a92ae
Jorge1 <agullojorge@gmail.com>
2023-11-27 11:27:16 +0100

    Added get clients

--- src/main/java/com/softyorch/cursospring/app/controllers/ClienteRestController.java
+++ src/main/java/com/softyorch/cursospring/app/controllers/ClienteRestController.java
@@ -1,14 +1,19 @@
 package com.softyorch.cursospring.app.controllers;
 
+import com.softyorch.cursospring.app.models.entity.Cliente;
 import com.softyorch.cursospring.app.service.IClienteService;
 import com.softyorch.cursospring.app.view.xml.ClienteList;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.util.List;
+
+@CrossOrigin(origins = {"http://localhost:4200"})
 @RestController
-@RequestMapping("/api/clientes")
+@RequestMapping("/api")
 public class ClienteRestController {
 
     @Autowired
@@ -17,6 +22,11 @@ public class ClienteRestController {
     @GetMapping(value = "/listar")
     public ClienteList listar() {
         return new ClienteList(clienteService.findAll());
+    }
+
+    @GetMapping(value = "/clients")
+    public List<Cliente> clients() {
+        return clienteService.findAll();
     }
 
 }
